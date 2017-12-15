4d3b961537c479c631b1f6c4d4c7aa3fbc0d7f70
Bengt Marten Agren <maraingist@me.com>
2017-12-16 02:47:52 +0800

    Initial version of paper-tape translator.

---
+++ PTF2C.hs
@@ -0,0 +1,71 @@
+module Main (main) where
+
+import System.FilePath
+import System.Environment
+import Control.Monad
+import Data.List
+import Data.List.Split
+import Data.Either
+import Numeric
+
+type Byte = Int
+
+main :: IO ()
+main = do
+    [fn] <- getArgs
+    let progname = takeBaseName fn
+    ss <- loadPaperTape fn
+    writeFile (progname <.> "h") $ unlines $ formatHeader progname ss
+    writeFile (progname <.> "c") $ unlines $ formatOutput progname ss
+
+loadPaperTape :: FilePath -> IO [(Int, [Byte])]
+loadPaperTape fn = do
+    ([n], ss) <- fmap (partitionEithers . map (uncurry processLine) . zip [1..] . lines) $ readFile fn
+    unless (length ss == n) $ error $ "record count mismatch: " ++ show (length ss) ++ "/=" ++ show n
+    return ss
+ 
+processLine :: Int -> String -> Either Int (Int, [Byte])
+processLine i s
+    | (';':'0':'0':c3:c2:c1:c0:xs) <- s = Left $ hex2int [c3,c2,c1,c0] -- ignoring checksum on count (hex2int xs)
+    | (';':lh:ll:a3:a2:a1:a0:xs) <- s = Right $ processRecord i (hex2int [lh,ll]) (hex2int [a3,a2,a1,a0]) xs
+    | otherwise = error $ "bad input line: '" ++ s ++ "'"
+
+processRecord :: Int -> Int -> Int -> String -> (Int, [Byte])
+processRecord i l a xs = (a, processBytes i (hex2int (drop (l*2) xs) - l - a) (take (l*2) xs))
+
+processBytes :: Int -> Int -> String -> [Byte]
+processBytes i cksum xs
+    | cksum' == cksum = bs
+    | otherwise = error $ "checksum error: " ++ show cksum' ++ "/=" ++ show cksum ++ " on line " ++ show i
+    where bs = map hex2int $ chunksOf 2 xs
+          cksum' = sum bs
+
+hex2int :: String -> Int
+hex2int = read . ("0x"++)
+
+int2hex :: Int -> String
+int2hex = ("0x"++) . (`showHex` "")
+
+formatHeader :: String -> [(Int, [Byte])] -> [String]
+formatHeader progname xs@((start,_):_) =
+    [ "#pragma once"
+    , ""
+    , "const unsigned " ++ progname ++ "_start = " ++ int2hex start ++ ";"
+    , "const unsigned " ++ progname ++ "_size = " ++ int2hex size ++ ";"
+    , ""
+    , "extern const unsigned char " ++ progname ++ "_text[];"
+    , ""
+    ] 
+    where size = sum $ map (length . snd) xs
+
+formatOutput :: String -> [(Int, [Byte])] -> [String]
+formatOutput progname xs@((start,_):_) =
+    [ "#include \"" ++ progname ++ ".h\""
+    , ""
+    , "const unsigned char " ++ progname ++ "_text[] ="
+    ]  ++
+    [ "    " ++ (if i > 0 then ", " else "{ ") ++ intercalate ", " (map int2hex bs)
+    | (i, (a, bs)) <- zip [0..] xs
+    ] ++
+    [ "    };" ]
+
